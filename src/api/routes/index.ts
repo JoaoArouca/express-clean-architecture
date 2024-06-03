1f2372c362f3c0196c7539874b1d99ac1b96f4cb
JoaoArouca <jarouca21@gmail.com>
2024-06-03 00:14:43 -0300

    create router

---
+++ src/api/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router, Request, Response } from "express"
+
+const productsRoutes = Router()
+
+productsRoutes.post('/create-product', (request: Request, response: Response) => {
+    return response.status(200).json("OK")
+})
+
+export { productsRoutes }
--- src/api/server.ts
+++ src/api/server.ts
@@ -1,8 +1,12 @@
 import express, { Request, Response } from 'express'
-import { env } from './shared/env'
+import { env } from '../config/env'
+import { productsRoutes } from './routes'
 
 const app = express()
 const PORT = env.PORT
+
+app.use(express.json())
+app.use(productsRoutes)
 
 app.listen(PORT, () => {
     console.log(`🚀 server is running on port: ${PORT}`)
